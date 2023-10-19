e3c20bf0adf2f6397f928ab6223e12f02a97c640
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-19 12:53:13 +0200

    done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+let bulb = document.querySelector("#bulb")
+
+function turnBulb(){
+    if(bulb.classList.contains("bulb-off")){
+        bulb.classList.replace("bulb-off", "bulb-on");
+    } else {
+        bulb.classList.replace("bulb-on", "bulb-off");
+    }
+
+}
